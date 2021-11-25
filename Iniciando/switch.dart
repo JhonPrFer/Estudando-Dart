61eb6e97a37c05bde794d97dddcfe41fb609a859
João <joao.pedro.fernandes.122@gmail.com>
2021-11-24 23:00:14 -0300

    usando switch

---
+++ Iniciando/switch.dart
@@ -0,0 +1,31 @@
+void main(){
+
+  int diaSemana = 3;
+
+  switch(diaSemana){
+    case 1:
+      print("Domingo");
+      break;
+    case 2:
+      print("Segunda");
+      break;
+    case 3:
+      print("Terça");
+      break;
+    case 4:
+      print("Qaurta");
+      break;
+    case 5:
+      print("Quinta");
+      break;
+    case 6:
+      print("Sexta");
+      break;  
+    case 7:
+      print("Sábado");
+      break;
+    default:
+      print("Dia da seman não existe use de 1 a 7");
+      break;
+  }
+}
